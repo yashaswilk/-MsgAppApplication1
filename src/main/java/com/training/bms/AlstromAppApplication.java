309e2346e0425eb0618dd7902fec1b70101533c5
Yashaswi Kotian <ylkotian@Yashaswis-MacBook-Pro.local>
2024-07-12 10:09:49 +0530

    first commit

---
+++ src/main/java/com/training/bms/AlstromAppApplication.java
@@ -0,0 +1,13 @@
+package com.training.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlstromAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlstromAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/bms/AlstromAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.bms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlstromAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
