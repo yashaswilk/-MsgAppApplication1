2b27c823e018fe2e7df19ae834374c0bf69f2aaf
yashaswilk <yashaswilk@gmail.com>
2024-07-12 12:46:10 +0530

    changesdone

--- src/test/java/com/training/bms/AlstromAppApplicationTests.java
+++ src/test/java/com/training/bms/AlstromAppApplicationTests.java
@@ -1,13 +1,16 @@
 package com.training.bms;
 
+
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class AlstromAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(199,200);
 	}
 
 }
